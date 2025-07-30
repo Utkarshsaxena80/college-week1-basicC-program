e72034d7b29faa19c9296ec37e7809ed0ede0c96
Utkarsh saxena <151988496+Utkarshsaxena80@users.noreply.github.com>
2025-07-30 11:29:41 +0530

    adding the code for q1(reverse the 3 digit number)

--- MAIN.C
+++ MAIN.C
@@ -1,5 +1,17 @@
 #include<stdio.h>
 
-int main(){
-    printf("hello");
+int main (){
+int n=0;
+printf("enter the 3 digit number ");
+scanf("%d",&n);
+int m=0;
+
+while(n!=0){
+int temp=n%10;
+m+=temp*10;
+n/=10;
+
+}
+printf("%d",m);
+return 0;
 }
